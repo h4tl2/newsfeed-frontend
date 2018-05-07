09e52aeb69ddce4f44d909e15aa80c51d2fd4320
Worameth Semapat <worameth.semapat@gmail.com>
2018-05-07 12:36:01 +0700

    working on ChatRoom Scrollview

--- src/ChatRoom.js
+++ src/ChatRoom.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Header, Text, Item, Container, Content, Left, Body, Right, Button, Icon } from 'native-base';
+import { View, StyleSheet, KeyboardAvoidingView, TouchableOpacity } from 'react-native';
+import { Header, Text, Item, Container, Content, Left, Body, Right, Button, Icon, Footer, FooterTab, Input } from 'native-base';
 import { rootStyle, variableStyle } from '../shared/app.style';
 
 export default class ChatRoom extends React.PureComponent {
     constructor() {
         super();
         this.state = {
-
+            message: '',
+            mockMessage: [
+                { self: false, message: 'Lorem ipsum dolor sit amet, ne case veri delicatissimi mea. His porro aperiam ceteros no' },
+                { self: false, message: 'At cum purto nemore impetus, case tantas invidunt cu qui' },
+                { self: true, message: 'Thanks!' },
+                { self: true, message: 'Again please!' },
+                { self: false, message: 'At cum purto nemore impetus, case tantas invidunt cu qui' },
+            ]
         }
     }
 
@@ -15,11 +22,25 @@ export default class ChatRoom extends React.PureComponent {
         this.props.navigation.goBack();
     }
 
+    onChangeMessage = message => {
+        this.setState({ message });
+    }
+
+    onSendMessage = () => {
+        mockMessage = this.state.mockMessage;
+        mockMessage.push({
+            self: true,
+            message: this.state.message
+        })
+        this.refs.contentView._root.scrollToEnd({animated: true});
+        this.setState({ message: '', mockMessage })
+    }
+
     render() {
         return (
             <Container>
                 <Header style={rootStyle.headerStyle} />
-                <Content>
+                <Content style={{ backgroundColor: '#FFF' }} ref="contentView" >
                     <Item style={[rootStyle.headerBG]}>
                         <Left>
                             <Button transparent onPress={this.onPressBackButton}>
@@ -31,14 +52,60 @@ export default class ChatRoom extends React.PureComponent {
                         </Text>
                         <Right />
                     </Item>
+                    {/*Chat Content*/}
+                    <View style={styles.container} >
+                        {
+                            this.state.mockMessage.map((data, key) => {
+                                return (
+                                    <View key={key} style={[styles.chatBox, data.self ? styles.chatBoxSelf : '']}>
+                                        <Text numberOfLines={10}>{data.message}</Text>
+                                    </View>
+                                );
+                            })
+                        }
+                    </View>
                 </Content>
+                <KeyboardAvoidingView behavior="padding" >
+                    <Item rounded style={[styles.noBorderBottom, { height: 35, backgroundColor: '#FFF', margin: 5 }]}>
+                        <Input placeholder='Type Message...' onChangeText={this.onChangeMessage} value={this.state.message} />
+                        <TouchableOpacity>
+                            <Icon name="md-happy" />
+                        </TouchableOpacity>
+                        {
+                            this.state.message.length > 0 &&
+                            <TouchableOpacity onPress={this.onSendMessage}>
+                                <Icon name="md-send" style={{ color: 'blue' }} />
+                            </TouchableOpacity>
+                        }
+                    </Item>
+                </KeyboardAvoidingView>
             </Container>
         );
     }
 }
 
 const styles = StyleSheet.create({
+    noBorderBottom: {
+        borderBottomWidth: 0,
+    },
     headerTitle: {
         fontSize: 16
+    },
+    container: {
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+    },
+    chatBox: {
+        maxWidth: '60%',
+        paddingVertical: 9,
+        paddingHorizontal: 13,
+        backgroundColor: '#E4E8EB',
+        borderRadius: 25,
+        alignSelf: 'flex-start',
+        marginVertical: 3
+    },
+    chatBoxSelf: {
+        backgroundColor: '#BEF18C',
+        alignSelf: 'flex-end',
     }
 });
